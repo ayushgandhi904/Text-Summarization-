79a4b28feb1171cbb92f07acad206df2445dd0c7
Your Name <ayushgandhi904@gmail.com>
2023-07-30 12:17:37 +0530

    data ingestion completed

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Text-Summarization-
 Summarizes the stories, long dialogues, paragraph etc. (Natural Language Processing (NLP) Project) 
+
+## Workflows
+
+1. Update config.yaml
+2. Update params.yaml
+3. Update entity.yaml
+4. Update configuration manager in src config
+5. Update components
+6. Update pipeline
+7. Update main.py
+8. Update app.py
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from text_summarizer.logging import logger
+from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+
+STAGE_NAME = "Data Ingestion"
+try:
+    logger.info(f"---------- {STAGE_NAME} ----------")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"====={STAGE_NAME} completed =====")
+
+except Exception as e:
+    logger.exception(e)
+    raise e

f81650e1026523701e190c62b41b47d46e11f7b2
Your Name <ayushgandhi904@gmail.com>
2023-07-30 13:33:47 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from text_summarizer.logging import logger
 from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from text_summarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 
 
 
@@ -8,6 +8,17 @@ STAGE_NAME = "Data Ingestion"
 try:
     logger.info(f"---------- {STAGE_NAME} ----------")
     data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"====={STAGE_NAME} completed =====")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation"
+try:
+    logger.info(f"---------- {STAGE_NAME} ----------")
+    data_ingestion = DataValidationPipeline()
     data_ingestion.main()
     logger.info(f"====={STAGE_NAME} completed =====")
 
---
+++ src/text_summarizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+from text_summarizer.config.configuration import ConfigurationManager
+from text_summarizer.components.data_validation import DataValiadtion
+from text_summarizer.logging import logger
+
+class DataValidationPipeline:
+    
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()

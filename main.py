d530a85862023d893c1c9cbf35b7b97f199b3ee1
Your Name <ayushgandhi904@gmail.com>
2023-07-30 17:47:32 +0530

    Data Transformation completed:

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from text_summarizer.logging import logger
 from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from text_summarizer.pipeline.stage_02_data_validation import DataValidationPipeline
+from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 
@@ -18,8 +19,19 @@ except Exception as e:
 STAGE_NAME = "Data Validation"
 try:
     logger.info(f"---------- {STAGE_NAME} ----------")
-    data_ingestion = DataValidationPipeline()
-    data_ingestion.main()
+    data_validation = DataValidationPipeline()
+    data_validation.main()
+    logger.info(f"====={STAGE_NAME} completed =====")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation"
+try:
+    logger.info(f"---------- {STAGE_NAME} ----------")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f"====={STAGE_NAME} completed =====")
 
 except Exception as e:

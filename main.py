bd0b5339ab2fa6edd0078f88e1417956d5bc0ea9
Your Name <ayushgandhi904@gmail.com>
2023-08-03 09:13:57 +0530

    Model Trainner completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from text_summarizer.logging import logger
 from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from text_summarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from text_summarizer.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 
 
 
@@ -32,6 +33,17 @@ try:
     logger.info(f"---------- {STAGE_NAME} ----------")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f"====={STAGE_NAME} completed =====")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer"
+try:
+    logger.info(f"---------- {STAGE_NAME} ----------")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.main()
     logger.info(f"====={STAGE_NAME} completed =====")
 
 except Exception as e:
